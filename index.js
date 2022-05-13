33878585039997c1233cd567bf8dbfd41fa3cc52
Metolix <95403304+Metolix@users.noreply.github.com>
2022-05-14 00:26:06 +0400

    Updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ speechBtn = document.querySelector(".speech"),
 copyBtn = document.querySelector(".copy"),
 twitterBtn = document.querySelector(".twitter"),
 synth = speechSynthesis;
+
 function randomQuote(){
     quoteBtn.classList.add("loading");
     quoteBtn.innerText = "Loading Quote...";
@@ -15,6 +16,7 @@ function randomQuote(){
         quoteBtn.innerText = "New Quote";
     });
 }
+
 speechBtn.addEventListener("click", ()=>{
     if(!quoteBtn.classList.contains("loading")){
         let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
@@ -24,11 +26,14 @@ speechBtn.addEventListener("click", ()=>{
         }, 10);
     }
 });
+
 copyBtn.addEventListener("click", ()=>{
     navigator.clipboard.writeText(quoteText.innerText);
 });
+
 twitterBtn.addEventListener("click", ()=>{
     let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
     window.open(tweetUrl, "_blank");
 });
+
 quoteBtn.addEventListener("click", randomQuote);
