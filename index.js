ebae784252eeb114fa2753f73033ea825792ce6e
Metolix <95403304+Metolix@users.noreply.github.com>
2022-05-14 00:22:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ speechBtn = document.querySelector(".speech"),
 copyBtn = document.querySelector(".copy"),
 twitterBtn = document.querySelector(".twitter"),
 synth = speechSynthesis;
-
 function randomQuote(){
     quoteBtn.classList.add("loading");
     quoteBtn.innerText = "Loading Quote...";
@@ -16,7 +15,6 @@ function randomQuote(){
         quoteBtn.innerText = "New Quote";
     });
 }
-
 speechBtn.addEventListener("click", ()=>{
     if(!quoteBtn.classList.contains("loading")){
         let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
@@ -26,14 +24,11 @@ speechBtn.addEventListener("click", ()=>{
         }, 10);
     }
 });
-
 copyBtn.addEventListener("click", ()=>{
     navigator.clipboard.writeText(quoteText.innerText);
 });
-
 twitterBtn.addEventListener("click", ()=>{
     let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
     window.open(tweetUrl, "_blank");
 });
-
 quoteBtn.addEventListener("click", randomQuote);
